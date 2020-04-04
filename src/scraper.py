3bfd5b988ec3278c733a233b877f72d4c5412413
Daniel Corretja <daniel.corretja@gmail.com>
2020-04-04 19:50:56 +0200

    Update scraper.py
    ~ Implemented __get_next_page_link method

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs import BeautifulSoup
 
 class RecipesScraper():
 
@@ -9,27 +9,33 @@ class RecipesScraper():
         self.url = "https://www.recetasgratis.net"
         self.data = pd.DataFrame(columns=['Id','Categoria','Nombre','Valoracion','Dificultad','NumComensales','Tiempo','Tipo','Descripcion'])
 
-
     def __download_html(self, url):
-        #Decarga una página HTML y la guarda en memoria
-		return requests.get(url).text
+        #Decarga una página HTML y la devuelve
+        return requests.get(url).text
 
+    def __download_html_and_parse(self, url):
+        #Decarga una página HTML, la parsea, y la devuelve
+		return BeautifulSoup(self.__download_html(url).text, 'html.parser')
 
     def __get_recipes_category_info(self, bs):
         #Devuelve lista de tuples [(nombre_cat1, link1), (nombre_cat2, link2)]
         categories = []
         #Obtenemos categorias principales en las que se clasifican las recetas
-        divs_main = bs.findAll("div", {"class": "categoria ga", "data-category":"Portada"})
+        div_categories = bs.findAll("div", {"class": "categoria ga", "data-category":"Portada"})
         #Iteramos cada categoria para obtener: link y nombre de la categoria
-        for category_div in divs_main:
-            link_category = categoria.findAll("a", {"class":"titulo"})
-            for link in link_category:
+        for div_category in div_categories:
+            a_category = div_category.findAll("a", {"class":"titulo"})
+            for link in a_category:
                 categories.append((link.getText(), link.attrs["href"]))
         return categories
 
     def __get_next_page_link(self, bs):
-        #Devuelve string con la url a la siguiente pagina. En caso de no existir devuelve None
-        #TODO
+        #Devuelve un string con la url a la siguiente pagina. En caso de no existir devuelve None
+        div_paginator = bs.find("div", {"class": "paginator"})
+        a_next_page = paginator.find("span", {"class":"current"}).find_next_sibling("a")
+        if (a_next_page is not None):
+            return a_next_page.attrs["href"]
+        return None
 
     def __get_recipes(self, bs, recipe_category):
         #Devuelve dataframe con la información de las recetas (incluyendo la categoria pasada por parámetro)
@@ -37,19 +43,17 @@ class RecipesScraper():
 
     def scrape(self):
         #Extrae la información de las recetas y la almacena en memoria
-        html_main = self.__download_html(self.url)
-        bs_main = BeautifulSoup(html_main, 'html.parser')
+        bs_main = self.__download_html_and_parse(self.url)
         recipes_category_info = self.__get_recipes_category_info(bs_main)
         for recipes_category_info_item in recipes_category_info:
             recipe_category = recipes_category_info_item[0]
             link = recipes_category_info_item[1]
             there_are_more_recipes = True
             while there_are_more_recipes:
-                html_recipes = self.__download_html(link)
-                bs_recipes = BeautifulSoup(html_recipes, 'html.parser')
-                recipes = __get_recipes(bs_recipes, recipe_category)
+                bs_recipes = self.__download_html_and_parse(link)
+                recipes = self.__get_recipes(bs_recipes, recipe_category)
                 self.data.append(recipes)
-                link = __get_next_page_link(bs_recipes)
+                link = self.__get_next_page_link(bs_recipes)
                 if link is None:
                     there_are_more_recipes = False
 
